1769838030ca0d2dc436abbb64c6b30ec76226c8
Darryl Walker <darryljwalker@me.com>
2024-01-29 07:47:33 +0000

    chore: success! restoring

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,6 +1,6 @@
 import getS3Files from '@/utils/getS3Files';
 
-// export const dynamic = 'force-dynamic'; // defaults to auto
+export const dynamic = 'force-dynamic'; // defaults to auto
 export async function GET() {
   const files = await getS3Files();
 
